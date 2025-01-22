622f0809cea2fd1e246d77a954a2b259db2275a8
Paula Ribeiro <contatopaular@gmail.com>
2025-01-22 07:49:51 -0300

    Adicionado as rotas

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,9 @@ import { Forgot } from "./pages/auth/Forgot";
 import { Home } from "./pages/app/Home";
 import 'antd/dist/reset.css';
 import { Profile } from "./pages/app/Profile";
+import { CreateSquad } from "./pages/app/telas/CreateSquad";
+import { CreateProject } from "./pages/app/telas/CreateProject";
+import { AddMember } from "./pages/app/telas/AddMember";
 
 function App() {
 
@@ -23,6 +26,9 @@ function App() {
           <Route path="/recuperar" element={<Forgot />} />
           <Route path="/inicio" element={<Home />} />
           <Route path="/perfil" element={<Profile />} />
+          <Route path="/criarsquad" element={<CreateSquad />} />
+          <Route path="/criarprojeto" element={<CreateProject />} />
+          <Route path="/adicionarmembo" element={<AddMember />} />
         </Routes>
       </AppProvider>
     </Router>
